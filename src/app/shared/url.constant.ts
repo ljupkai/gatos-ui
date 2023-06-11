dc0f8dd5adf0b19fb4f289bbc237a835770d0c42
ljupka <ljupka.ivanova@gmail.com>
2023-06-11 16:46:38 +0200

    deja url local

--- src/app/shared/url.constant.ts
+++ src/app/shared/url.constant.ts
@@ -1,4 +1,4 @@
-// export const BASE_URL = 'http://localhost:3000/';
+export const BASE_URL = 'http://localhost:3000/';
 
-export const BASE_URL = 'http://app:3000/';
+// export const BASE_URL = 'http://http://51.178.27.110:3000/';
 
